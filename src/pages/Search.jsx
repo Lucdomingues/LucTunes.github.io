28629d7060cf67e88f64a4eccdb0ad9a9b857309
Lucdomingues <lojalucc@outlook.com>
2022-09-03 18:08:02 -0300

    requisito 05

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,11 +2,41 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  state = {
+    search: '',
+  };
+
+  HandleInputSearch = ({ target }) => {
+    const { value } = target;
+    this.setState({
+      search: value,
+    });
+  };
+
   render() {
+    const { search } = this.state;
+    const MIN_NUMBER = 2;
     return (
-      <div data-testid="page-search">
+      <section data-testid="page-search">
         <Header />
-      </div>
+        <form>
+          <input
+            type="text"
+            name="pesquisa"
+            id="pes"
+            value={ search }
+            onChange={ this.HandleInputSearch }
+            data-testid="search-artist-input"
+          />
+          <button
+            type="button"
+            disabled={ !(search.length >= MIN_NUMBER && 'disabled') }
+            data-testid="search-artist-button"
+          >
+            Pesquisar
+          </button>
+        </form>
+      </section>
     );
   }
 }
