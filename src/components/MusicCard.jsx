7418563958ed9891b27ebb85bacfd33653e2b318
Lucdomingues <lojalucc@outlook.com>
2022-09-06 21:18:05 -0300

    requisito 08

--- src/components/MusicCard.jsx
+++ src/components/MusicCard.jsx
@@ -1,14 +1,38 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import getMusics from '../services/musicsAPI';
+import { addSong } from '../services/favoriteSongsAPI';
+
+import Loading from './Loading';
 
 class MusicCard extends React.Component {
+  state = {
+    loading: false,
+  };
+
+  HandleFavorites = async ({ target }) => {
+    this.loadingGo();
+    const objectId = await getMusics(target.id);
+    const favorite = await addSong(objectId);
+    this.loadingGo();
+    return favorite;
+  };
+
+  loadingGo = () => {
+    const { loading } = this.state;
+    this.setState({
+      loading: !loading,
+    });
+  };
+
   render() {
     const { arr } = this.props;
+    const { loading } = this.state;
     const copyArr = arr.slice([1]);
     return (
       <section>
         <ul>
-          {copyArr.map(({ trackName, previewUrl }) => (
+          {copyArr.map(({ trackName, previewUrl, trackId }) => (
             <li key={ trackName }>
               <p>{trackName}</p>
               <audio data-testid="audio-component" src={ previewUrl } controls>
@@ -16,9 +40,22 @@ class MusicCard extends React.Component {
                 O seu navegador não suporta elemento
                 <code>audio</code>
               </audio>
+              <form action="">
+                <label htmlFor={ trackId }>
+                  Favorita
+                  <input
+                    type="checkbox"
+                    onClick={ this.HandleFavorites }
+                    name="musicFavo"
+                    id={ trackId }
+                    data-testid={ `checkbox-music-${trackId}` }
+                  />
+                </label>
+              </form>
             </li>
           ))}
         </ul>
+        {loading && <Loading />}
       </section>
     );
   }
